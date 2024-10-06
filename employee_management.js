02dda5b5482b272af2b205473ff92356d41accfd
Jorge Bernal <jorgebernal@usf.edu>
2024-10-06 14:09:32 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -11,3 +11,18 @@ class Employee{
         return `${this.name} - ${this.position}: $${this.salary}`;
     }
 };
+
+// Task 2: Create a Department Class
+
+class Department{
+    constructor(name){
+        this.name = name;
+        this.employees = []; } //empty array for employees 
+
+addEmployee(employee){
+    this.employees.push(employee); //adds employee into the empty employees array
+}
+getDepartmentSalary(){
+    return this.employees.reduce((total, employee)=> {return total + employee.salary}, 0)
+}            
+}; // calculates department salary 
